41baf42027cde1d1cdb264d09d24380abe4f09be
Umut Keskin <umutkeskin014@gmail.com>
2024-01-10 14:09:08 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,158 @@
+function prj1(){prjimg
+    document.getElementById("prj2").style.width=200;
+    document.getElementById("prj2h").style.visibility="visible";
+    document.getElementById("prj2in").style.visibility="hidden";
+    document.getElementById("prjimg2").style.visibility="hidden";
+
+    document.getElementById("prj1").style.width=400;
+    document.getElementById("prj1h").style.visibility="hidden";
+    document.getElementById("prj1in").style.visibility="visible";
+    document.getElementById("prjimg").style.visibility="visible";
+
+    document.getElementById("prj3").style.width=200;
+    document.getElementById("prj3h").style.visibility="visible";
+    document.getElementById("prj3in").style.visibility="hidden";
+    document.getElementById("prjimg3").style.visibility="hidden";
+
+    document.getElementById("prj4").style.width=200;
+    document.getElementById("prj4h").style.visibility="visible";
+    document.getElementById("prj4in").style.visibility="hidden";
+    document.getElementById("prjimg4").style.visibility="hidden";
+
+    document.getElementById("prj5").style.width=200;
+    document.getElementById("prj5h").style.visibility="visible";
+    document.getElementById("prj5in").style.visibility="hidden";
+    document.getElementById("prjimg5").style.visibility="hidden";
+}
+function prj2(){
+    document.getElementById("prj2").style.width=400;
+    document.getElementById("prj2h").style.visibility="hidden";
+    document.getElementById("prj2in").style.visibility="visible";
+    document.getElementById("prjimg2").style.visibility="visible";
+
+    document.getElementById("prj1").style.width=200;
+    document.getElementById("prj1h").style.visibility="visible";
+    document.getElementById("prj1in").style.visibility="hidden";
+    document.getElementById("prjimg").style.visibility="hidden";
+
+    document.getElementById("prj3").style.width=200;
+    document.getElementById("prj3h").style.visibility="visible";
+    document.getElementById("prj3in").style.visibility="hidden";
+    document.getElementById("prjimg3").style.visibility="hidden";
+
+    document.getElementById("prj4").style.width=200;
+    document.getElementById("prj4h").style.visibility="visible";
+    document.getElementById("prj4in").style.visibility="hidden";
+    document.getElementById("prjimg4").style.visibility="hidden";
+
+    document.getElementById("prj5").style.width=200;
+    document.getElementById("prj5h").style.visibility="visible";
+    document.getElementById("prj5in").style.visibility="hidden";
+    document.getElementById("prjimg5").style.visibility="hidden";
+}
+function prj3(){
+    document.getElementById("prj2").style.width=200;
+    document.getElementById("prj2h").style.visibility="visible";
+    document.getElementById("prj2in").style.visibility="hidden";
+    document.getElementById("prjimg2").style.visibility="hidden";
+
+    document.getElementById("prj1").style.width=200;
+    document.getElementById("prj1h").style.visibility="visible";
+    document.getElementById("prj1in").style.visibility="hidden";
+    document.getElementById("prjimg").style.visibility="hidden";
+
+    document.getElementById("prj3").style.width=400;
+    document.getElementById("prj3h").style.visibility="hidden";
+    document.getElementById("prj3in").style.visibility="visible";
+    document.getElementById("prjimg3").style.visibility="visible";
+
+    document.getElementById("prj4").style.width=200;
+    document.getElementById("prj4h").style.visibility="visible";
+    document.getElementById("prj4in").style.visibility="hidden";
+    document.getElementById("prjimg4").style.visibility="hidden";
+
+    document.getElementById("prj5").style.width=200;
+    document.getElementById("prj5h").style.visibility="visible";
+    document.getElementById("prj5in").style.visibility="hidden";
+    document.getElementById("prjimg5").style.visibility="hidden";
+}
+function prj4(){
+    document.getElementById("prj2").style.width=200;
+    document.getElementById("prj2h").style.visibility="visible";
+    document.getElementById("prj2in").style.visibility="hidden";
+    document.getElementById("prjimg2").style.visibility="hidden";
+
+    document.getElementById("prj1").style.width=200;
+    document.getElementById("prj1h").style.visibility="visible";
+    document.getElementById("prj1in").style.visibility="hidden";
+    document.getElementById("prjimg").style.visibility="hidden";
+
+    document.getElementById("prj3").style.width=200;
+    document.getElementById("prj3h").style.visibility="visible";
+    document.getElementById("prj3in").style.visibility="hidden";
+    document.getElementById("prjimg3").style.visibility="hidden";
+
+    document.getElementById("prj4").style.width=400;
+    document.getElementById("prj4h").style.visibility="hidden";
+    document.getElementById("prj4in").style.visibility="visible";
+    document.getElementById("prjimg4").style.visibility="visible";
+
+    document.getElementById("prj5").style.width=200;
+    document.getElementById("prj5h").style.visibility="visible";
+    document.getElementById("prj5in").style.visibility="hidden";
+    document.getElementById("prjimg5").style.visibility="hidden";
+}
+function prj5(){
+    document.getElementById("prj2").style.width=200;
+    document.getElementById("prj2h").style.visibility="visible";
+    document.getElementById("prj2in").style.visibility="hidden";
+    document.getElementById("prjimg2").style.visibility="hidden";
+
+    document.getElementById("prj1").style.width=200;
+    document.getElementById("prj1h").style.visibility="visible";
+    document.getElementById("prj1in").style.visibility="hidden";
+    document.getElementById("prjimg").style.visibility="hidden";
+
+    document.getElementById("prj3").style.width=200;
+    document.getElementById("prj3h").style.visibility="visible";
+    document.getElementById("prj3in").style.visibility="hidden";
+    document.getElementById("prjimg3").style.visibility="hidden";
+
+    document.getElementById("prj4").style.width=200;
+    document.getElementById("prj4h").style.visibility="visible";
+    document.getElementById("prj4in").style.visibility="hidden";
+    document.getElementById("prjimg4").style.visibility="hidden";
+
+    document.getElementById("prj5").style.width=400;
+    document.getElementById("prj5h").style.visibility="hidden";
+    document.getElementById("prj5in").style.visibility="visible";
+    document.getElementById("prjimg5").style.visibility="visible";
+}
+
+function about(){
+    window.scrollTo({
+        top: window.innerHeight,
+        behavior: 'smooth'
+      });
+}
+
+function skills(){
+    window.scrollTo({
+        top: 2 * window.innerHeight,
+        behavior: 'smooth'
+      });
+}
+
+function projects(){
+    window.scrollTo({
+        top: 3 * window.innerHeight,
+        behavior: 'smooth'
+      });
+}
+
+function uk(){
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+}
